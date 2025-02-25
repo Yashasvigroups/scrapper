8fd9172b15577060119e438e4b140f02ee7b4260
devdutt6 <devduttchudasama1339@gmail.com>
2025-02-26 02:17:05 +0530

    style: :poop: simple refactor \n added dependency

--- handlers/panHandlers.js
+++ handlers/panHandlers.js
@@ -1,11 +1,11 @@
-const { Pan } = require('../schema/panCards.schema');
+const { Pan } = require("../schema/panCards.schema");
 
 async function addPAN(req, res) {
   try {
     const { panNumber } = req.body;
 
     if (!panNumber) {
-      res.status(400).json({ message: 'enter pan number to add' });
+      res.status(400).json({ message: "enter pan number to add" });
       return;
     }
 
@@ -13,16 +13,16 @@ async function addPAN(req, res) {
       panNumber,
     });
     if (isAlreadyAdded > 0) {
-      res.status(409).json({ message: 'pan already in use by someone' });
+      res.status(409).json({ message: "pan already in use by someone" });
       return;
     }
 
     await Pan.create({ panNumber, userId: req.userId });
 
-    res.status(201).json({ message: 'pan added successfully' });
+    res.status(201).json({ message: "pan added successfully" });
   } catch (err) {
-    console.log('Failed adding pan: ', err?.message);
-    res.status(500).json({ message: 'Internal Server Error' });
+    console.log("Failed adding pan: ", err?.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
 
@@ -32,8 +32,8 @@ async function getPANs(req, res) {
 
     res.status(200).json({ pans });
   } catch (err) {
-    console.log('Failed get pans: ', err?.message);
-    res.status(500).json({ message: 'Internal Server Error' });
+    console.log("Failed get pans: ", err?.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
 
@@ -42,7 +42,7 @@ async function deletePAN(req, res) {
     const { panNumber } = req.body;
 
     if (!panNumber) {
-      res.status(400).json({ message: 'enter pan number to delete' });
+      res.status(400).json({ message: "enter pan number to delete" });
       return;
     }
 
@@ -51,16 +51,16 @@ async function deletePAN(req, res) {
       userId: req.userId,
     });
     if (!isRegistered) {
-      res.status(404).json({ message: 'pan not found' });
+      res.status(404).json({ message: "pan not found" });
       return;
     }
 
     await Pan.deleteOne({ panNumber, userId: req.userId });
 
-    res.status(200).json({ message: 'pan deleted successfully' });
+    res.status(200).json({ message: "pan deleted successfully" });
   } catch (err) {
-    console.log('Failed deleting pan: ', err?.message);
-    res.status(500).json({ message: 'Internal Server Error' });
+    console.log("Failed deleting pan: ", err?.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
 
