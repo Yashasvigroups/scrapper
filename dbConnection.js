e9829a1ad323cf8c67c5db42038377f3440b2f80
devdutt6 <devduttchudasama1339@gmail.com>
2025-02-27 12:39:34 +0530

    refactor: :sparkles: production db connection

--- dbConnection.js
+++ dbConnection.js
@@ -1,16 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose
-  .connect(process.env.MONGO_URI || 'mongodb://localhost:27017/admin', {
-    dbName: process.env.DB_NAME || 'ipo',
-    auth: {
-      username: process.env.DB_USER || 'admin',
-      password: process.env.DB_PASSWORD || 'password',
-    },
-  })
-  .then(() => {
-    console.log('DB connection established');
-  })
-  .catch((err) => {
-    console.log('Failed to connect to db ', err);
-  });
+if (process.env.NODE_ENV !== "production") {
+  mongoose
+    .connect(process.env.MONGO_URI || "mongodb://localhost:27017/admin", {
+      dbName: process.env.DB_NAME || "ipo",
+      auth: {
+        username: process.env.DB_USER || "admin",
+        password: process.env.DB_PASSWORD || "password",
+      },
+    })
+    .then(() => {
+      console.log("DB connection established");
+    })
+    .catch((err) => {
+      console.log("Failed to connect to db ", err);
+    });
+} else {
+  mongoose
+    .connect(process.env.MONGO_URI, { dbName: process.env.DB_NAME })
+    .then(() => {
+      console.log("DB connection established");
+    })
+    .catch((err) => {
+      console.log("Failed to connect to db ", err);
+    });
+}
