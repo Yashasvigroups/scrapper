6ff9039d76aaaed4416f87cb8d65cd438328f6fe
devdutt6 <devduttchudasama1339@gmail.com>
2025-03-05 15:12:52 +0530

    refactor: :zap: mordenized connection

--- dbConnection.js
+++ dbConnection.js
@@ -1,18 +1,16 @@
 const mongoose = require("mongoose");
 
 let connectDb = async () => {
+  // waiting for mongo container to start
   await new Promise((res, _) => setTimeout(res, 3000));
   console.log("##################### DB Connection");
-
-  mongoose
-    .connect(process.env.MONGO_URI, {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
       dbName: process.env.DB_NAME || "allotment",
-    })
-    .then(() => {
-      console.log("DB connection established");
-    })
-    .catch((err) => {
-      console.log("Failed to connect to db ", err);
     });
+    console.log("DB connection established");
+  } catch (err) {
+    console.log("Failed to connect to db ", err);
+  }
 };
 connectDb();
